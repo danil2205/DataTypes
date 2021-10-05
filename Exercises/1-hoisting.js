c1b1ecb21844b4db258c678ca989767d028d4130
danil2205 <04danil@gmail.com>
2021-10-05 22:44:47 +0300

    Labs 1-4

--- Exercises/1-hoisting.js
+++ Exercises/1-hoisting.js
@@ -1,5 +1,12 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unreachable */
+/* eslint-disable no-var */
 'use strict';
 
-const fn = null;
+
+const fn = () => {
+  console.log({ a });
+  var a = 228;
+};
 
 module.exports = { fn };
--- Exercises/4-count-types.js
+++ Exercises/4-count-types.js
@@ -1,5 +1,12 @@
 'use strict';
-
-const countTypesInArray = null;
+const countTypesInArray = (array) => {
+  const counters = {};
+  for (const element of array) {
+    const type = typeof element;
+    const count = counters[type] || 0;
+    counters[type] = count + 1;
+  }
+  return counters;
+};
 
 module.exports = { countTypesInArray };
